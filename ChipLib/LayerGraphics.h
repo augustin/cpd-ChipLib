554338ff556229eaca07e9059b59076e111378f1
Augustin Cavalier <augustin.j.cavalier@gmail.com>
2013-12-24 16:41:25 -0500

    Large refactor, WIRE command, drawing bug fixes.

--- ChipLib/LayerGraphics.h
+++ ChipLib/LayerGraphics.h
@@ -1,12 +1,18 @@
 #ifndef LAYERGRAPHICS_H
 #define LAYERGRAPHICS_H
 
-#include <qglobal.h>
+#include <QtGlobal>
+#include <QList>
+#include <QPair>
+
+typedef QPair<qint64, qint64> Point;
+typedef QList<Point> PointList;
 
 class LayerGraphics
 {
 public:
     virtual void rect(qint64 length, qint64 width, qint64 xpos, qint64 ypos, qint64 rotation = 0) = 0;
+    virtual void line(PointList p) = 0;
 };
 
 #endif // LAYERGRAPHICS_H
