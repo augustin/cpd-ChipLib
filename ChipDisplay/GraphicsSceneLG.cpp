87a910e61f89cca484dadd8dc087bde563848606
Augustin Cavalier <augustin.j.cavalier@gmail.com>
2013-12-26 15:07:05 -0500

    Fix selectable.

--- ChipDisplay/GraphicsSceneLG.cpp
+++ ChipDisplay/GraphicsSceneLG.cpp
@@ -10,8 +10,8 @@ GraphicsSceneLG::GraphicsSceneLG(QGraphicsScene *g)
 
 void GraphicsSceneLG::rect(qint64 length, qint64 width, qint64 xpos, qint64 ypos)
 {
-    QGraphicsItem* i = gs->addRect(xpos-(length/2), ypos-(width/2), length, width);
-    i->setFlag(QGraphicsItem::ItemIsFocusable);
+    gs->addRect(xpos-(length/2), ypos-(width/2), length, width)
+            ->setFlag(QGraphicsItem::ItemIsFocusable);
 }
 
 void GraphicsSceneLG::line(PointList p)
@@ -20,5 +20,5 @@ void GraphicsSceneLG::line(PointList p)
     foreach(Point pnt, p) {
         path.lineTo(pnt.first, pnt.second);
     }
-    gs->addPath(path);
+    gs->addPath(path)->setFlag(QGraphicsItem::ItemIsFocusable);
 }
