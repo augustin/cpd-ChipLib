469eaddb7975511fb788cce16574fb7150ac1d45
waddlesplash <ajcsweb@gmail.com>
2013-12-27 11:02:16 -0500

    Add version number to Globals.h

--- ChipLib/Globals.h
+++ ChipLib/Globals.h
@@ -1,12 +1,13 @@
-#ifndef CIFREAD_H
-#define CIFREAD_H
+#ifndef CHIPLIB_GLOBALS_H
+#define CHIPLIB_GLOBALS_H
 
 class QGraphicsView;
 class QString;
 class QByteArray;
 
+#define CHIPLIB_VER "0.1.0-devel"
 #define LOG(type, line, desc) qDebug("%s:\t%d\t%s", type, line, qPrintable(desc))
 
 QByteArray qFileGetContents(QString fileName);
 
-#endif // CIFREAD_H
+#endif // CHIPLIB_GLOBALS_H
