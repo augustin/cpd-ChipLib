dffff3c1edf0f64789cc524d08d248dc9d373f69
Augustin Cavalier <augustin.j.cavalier@gmail.com>
2014-01-02 14:12:29 -0500

    Fixes to the PainterLG.

--- ChipDisplay/PainterLG.cpp
+++ ChipDisplay/PainterLG.cpp
@@ -5,9 +5,20 @@
 PainterLG::PainterLG(QPainter *painter)
 {
     p = painter;
+    p->scale(1.0, -1.0);
 }
 
 void PainterLG::rect(qint64 length, qint64 width, qint64 xpos, qint64 ypos)
 {
-    p->drawRect(xpos, ypos, length, width);
+    p->drawRect(xpos-(length/2), ypos-(width/2), length, width);
 }
+
+void PainterLG::line(PointList pnts)
+{
+    QPainterPath path(QPointF(pnts[0].first, pnts[0].second));
+    foreach(Point pnt, pnts) {
+        path.lineTo(pnt.first, pnt.second);
+    }
+    p->drawPath(path);
+}
+
