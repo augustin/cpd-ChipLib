65c9395c5e48a24ad4f3b2a3374c92040fc2c81d
waddlesplash <ajcsweb@gmail.com>
2014-01-24 20:45:53 -0500

    Lots of fixes to Chip and PainterLG about rendering.

--- ChipDisplay/PainterLG.h
+++ ChipDisplay/PainterLG.h
@@ -2,13 +2,16 @@
 #define PAINTERLG_H
 
 #include <LayerGraphics.h>
+#include <QRect>
+
+#define SCALE_FACTOR 50
 
 class QPainter;
 
 class PainterLG : public LayerGraphics
 {
 public:
-    PainterLG(QPainter* painter = 0, bool realLineWidths = false);
+	PainterLG(QPainter* painter = 0, bool realLineWidths = false, QRect bR = QRect());
 
     void line(PointList pnts, qint64 w);
     void poly(PointList pnts, qint64 w);
--- ChipLib/Chip.cpp
+++ ChipLib/Chip.cpp
@@ -7,10 +7,10 @@ ChipObject* ChipLayer::addRect(qint64 length, qint64 width, qint64 x, qint64 y)
     qint64 xpos = x-(length/2), ypos = y-(width/2);
     ChipObject* c = new ChipObject;
     c->type = POLYGON;
-    c->points.append(new Point(xpos, ypos));
-    c->points.append(new Point(xpos+length, ypos));
-    c->points.append(new Point(xpos+length, ypos+width));
-    c->points.append(new Point(xpos, ypos+width));
+	c->points.append(new Point(xpos, ypos));
+	c->points.append(new Point(xpos+length, ypos));
+	c->points.append(new Point(xpos+length, ypos+width));
+	c->points.append(new Point(xpos, ypos+width));
     c->w = 1;
     append(c);
     return c;
@@ -64,6 +64,22 @@ ChipLayer* Chip::layer(QString name)
     }
 }
 
+QList<ChipObject*>* Chip::getObjects(QString layer)
+{
+	QList<ChipObject*>* objects;
+	if(layer.size()) {
+		objects = objectsForLayers.value(layer);
+	} else {
+		objects = new QList<ChipObject*>;
+		foreach(ChipLayer* a, objectsForLayers.values()) {
+			for(int i = 0; i < a->size(); i++) {
+				objects->append(a->at(i));
+			}
+		}
+	}
+	return objects;
+}
+
 QMap<qint64, QString> Chip::countObjs()
 {
     QMap<qint64, QString> ret;
@@ -73,40 +89,33 @@ QMap<qint64, QString> Chip::countObjs()
     return ret;
 }
 
-QSize Chip::nativeLayerSize(QString layer)
+QRect Chip::boundingRect(QString layer)
 {
     /* ulc = upper left corner - lrc = lower right corner */
-    ChipLayer* items = objectsForLayers.value(layer);
-    qint64 ulcX = items->at(0)->points.at(0)->x,
-            ulcY = items->at(0)->points.at(0)->y,
-            lrcX = items->at(0)->points.at(0)->x,
-            lrcY = items->at(0)->points.at(0)->y;
+	QList<ChipObject*>* items = getObjects(layer);
+
+	qint64 ulcX = INT_MAX,
+		   ulcY = INT_MIN,
+		   lrcX = INT_MIN,
+		   lrcY = INT_MAX;
     for(int i = 0; i < items->size(); i++) {
         ChipObject* o = items->at(i);
         foreach(Point* p, o->points) {
-            if(p->x < ulcX) { ulcX = p->x; }
-            if(p->y < ulcY) { ulcY = p->y; }
+			if(p->x - o->w < ulcX) { ulcX = p->x - o->w; }
+			if(p->y + o->w > ulcY) { ulcY = p->y + o->w; }
 
-            if(p->x > ulcX) { lrcX = p->x; }
-            if(p->y > ulcY) { lrcY = p->y; }
+			if(p->x + o->w > lrcX) { lrcX = p->x + o->w; }
+			if(p->y - o->w < lrcY) { lrcY = p->y - o->w; }
         }
     }
-    return QSize(lrcX - ulcX, lrcY - ulcY);
+	/*if(ulcX < 0) { ulcX = 0; }
+	if(lrcY > 0) { lrcY = 0; }*/
+	return QRect(QPoint(ulcX, ulcY), QPoint(lrcX, lrcY));
 }
 
 void Chip::render(LayerGraphics *lg, QString layer, qint64 l)
 {
-    QList<ChipObject*>* objects;
-    if(layer.size()) {
-        objects = objectsForLayers.value(layer);
-    } else {
-        objects = new QList<ChipObject*>;
-        foreach(ChipLayer* a, objectsForLayers.values()) {
-            for(int i = 0; i < a->size(); i++) {
-                objects->append(a->at(i));
-            }
-        }
-    }
+	QList<ChipObject*>* objects = getObjects(layer);
 
     qint64 limit; // =  ? l : objects->size();
     if(l < 1) { l = objects->size(); }
