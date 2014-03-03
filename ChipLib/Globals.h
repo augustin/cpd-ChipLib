6b02f2519897d13ba322c3ada5220c955d2ca150
waddlesplash <ajcsweb@gmail.com>
2014-03-03 14:49:04 -0500

    v0.5.1!

--- ChipLib/Globals.h
+++ ChipLib/Globals.h
@@ -5,7 +5,7 @@ class QGraphicsView;
 class QString;
 class QByteArray;
 
-#define CHIPLIB_VER "0.5.0"
+#define CHIPLIB_VER "0.5.1"
 #define LOG(type, line, desc) qDebug("%s:\t%d\t%s", type, line, qPrintable(desc))
 
 QByteArray qFileGetContents(QString fileName);
