d0783652e26d15bcffaa9ed44b8089cbb7610c60
Augustin Cavalier <augustin.j.cavalier@gmail.com>
2014-03-06 19:35:40 -0500

    Fix the fencepost error in the other LGs.

--- ChipDisplay/GraphicsSceneLG.cpp
+++ ChipDisplay/GraphicsSceneLG.cpp
@@ -46,5 +46,7 @@ void GraphicsSceneLG::poly(PointList p, qint64 w)
 
 void GraphicsSceneLG::rect(qint64 x1, qint64 y1, qint64 x2, qint64 y2)
 {
+    (x1 < x2) ? (x2 -= 1) : (x1 -= 1);
+    (y1 < y2) ? (y2 -= 1) : (y1 -= 1);
     gs->addRect(x1, y1, x2-x1, y2-y1)->setFlag(QGraphicsItem::ItemIsFocusable);
 }
--- ChipDisplay/PainterLG.cpp
+++ ChipDisplay/PainterLG.cpp
@@ -54,5 +54,7 @@ void PainterLG::poly(PointList pnts, qint64 w)
 
 void PainterLG::rect(qint64 x1, qint64 y1, qint64 x2, qint64 y2)
 {
+    (x1 < x2) ? (x2 -= 1) : (x1 -= 1);
+    (y1 < y2) ? (y2 -= 1) : (y1 -= 1);
     p->drawRect(x1, y1, x2-x1, y1-y2);
 }
